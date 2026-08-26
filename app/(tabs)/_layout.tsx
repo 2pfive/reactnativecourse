fccbdffcb8e5f2cf978eb6c2e4e1cce12d2816fc
Isaac Akebe <akebe.isaac@ayokai.com>
2026-08-26 19:15:09 +0100

    :wq
     :wq
    
    :wq

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack, Tabs } from "expo-router"
+
+const TabLayout = () => {
+    return (
+        <Tabs screenOptions={{ headerShown: false }}>
+            <Tabs.Screen name="index" options={{ title: 'Home' }}></Tabs.Screen>
+            <Tabs.Screen name="subscriptions" options={{ title: 'Subscriptions' }}></Tabs.Screen>
+            <Tabs.Screen name="insights" options={{ title: 'Insights' }}></Tabs.Screen>
+            <Tabs.Screen name="settigns" options={{ title: 'Settings' }}></Tabs.Screen>
+            <Tabs.Screen name="subscriptions/[id]" options={{ href: null }}></Tabs.Screen>
+        </Tabs>
+    )
+}
+
+export default TabLayout
