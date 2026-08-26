99839bb5120e07426478fc76a2f55edb327b3148
Isaac Akebe <akebe.isaac@ayokai.com>
2026-08-26 19:27:43 +0100

    add assets folder

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -6,7 +6,7 @@ const TabLayout = () => {
             <Tabs.Screen name="index" options={{ title: 'Home' }}></Tabs.Screen>
             <Tabs.Screen name="subscriptions" options={{ title: 'Subscriptions' }}></Tabs.Screen>
             <Tabs.Screen name="insights" options={{ title: 'Insights' }}></Tabs.Screen>
-            <Tabs.Screen name="settigns" options={{ title: 'Settings' }}></Tabs.Screen>
+            <Tabs.Screen name="settings" options={{ title: 'Settings' }}></Tabs.Screen>
             <Tabs.Screen name="subscriptions/[id]" options={{ href: null }}></Tabs.Screen>
         </Tabs>
     )
